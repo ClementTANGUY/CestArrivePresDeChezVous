4360d9f9a57e8606121f88b4eaabb58ece76c939
ClementTANGUY <clemdelatang@gmail.com>
2020-04-01 22:43:10 +0200

    Add nav,footer,landing page, logo & css, create cases controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+
+  get 'cases/new'
+  get 'cases/create'
+  get 'cases/update'
+  get 'cases/edit'
+  get 'cases/destroy'
+  get 'cases/show'
+  get 'cases/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  get "/home" => "pages#home"
+  get "/about" => "pages#about"
+  get "/lexicon" => "pages#lexicon"
+
+  resources :products
+
 end
